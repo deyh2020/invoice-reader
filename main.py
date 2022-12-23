e4f08a4ad43610bb0b6eac99bc2cfdd830bf2da6
Alex <alex@likening.co.uk>
2022-12-23 23:24:50 +0000

    update

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import pytesseract
+import cv2
+import matplotlib.pyplot as plt
+import pdf2image
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+filename = 'img/invoice4.pdf'
+
+ext = filename.split('.')[-1]
+
+if ext == "pdf":
+    ...
+
+elif ext == "png" or ext == "jpg":
+    ...
+
+img = cv2.imread(filename)
+
+try:
+    h, w, _ = img.shape
+
+except AttributeError as e:
+    print("Unsupported file format: ", e)
+
+boxes = pytesseract.image_to_boxes(img)
+print(pytesseract.image_to_string(img))
+
+for b in boxes.splitlines():
+    b = b.split()
+    cv2.rectangle(img, ((int(b[1]), h - int(b[2]))), ((int(b[3]), h - int(b[4]))), (0, 255, 0), 2)
+
+plt.imshow(img)
+plt.show()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
